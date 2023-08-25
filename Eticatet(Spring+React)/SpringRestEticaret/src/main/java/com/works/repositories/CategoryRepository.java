4bb1dd383a6bce32cfc7996ff5a925c6ed6b9428
İsa HATİPOĞLU <113295965+isahatipoglu74@users.noreply.github.com>
2023-08-25 22:38:01 +0300

    Delete Eticatet(Spring+React)/SpringRestEticaret directory

--- Eticatet(Spring+React)/SpringRestEticaret/src/main/java/com/works/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.works.repositories;
-
-import com.works.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}

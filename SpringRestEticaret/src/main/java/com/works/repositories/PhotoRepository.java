cd01b4a19121a11d91d480ea8bb973bc95e96b40
İsa HATİPOĞLU <113295965+isahatipoglu74@users.noreply.github.com>
2023-08-25 22:02:34 +0300

    Delete SpringRestEticaret directory

--- SpringRestEticaret/src/main/java/com/works/repositories/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.works.repositories;
-
-import com.works.models.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
